96bed726c8f0af2481b48dab600c50f3b24f4b7a
Jeason <zhaojitong@xiaomi.com>
2017-07-23 21:55:39 +0800

    test

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // For detail about @mi/eslint-config-mcfe-react-app
+  // http://git.n.xiaomi.com/mcfe/create-mcfe-react-app/blob/master/packages/eslint-config-mcfe-react-app/index.js
+  extends: '@mi/eslint-config-mcfe-react-app',
+  rules: {
+    // add project level rules if need.
+  },
+}
